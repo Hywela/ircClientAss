3abb41391042c6f33f7833be5982c5c6eb8c3349
Hywela <Kriss_kb@hotmail.com>
2013-10-30 23:05:26 +0100

    adad

---
+++ java_irc_client/src/irc_client/Language.java
@@ -0,0 +1,49 @@
+package irc_client;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+
+/**
+ * file for handelig the finding and storing the lanugabe bundle spesified by user
+ * if no lanugage is chosen will look up default language.
+ * 
+ * @author Kristoffer
+ * @author Kevin
+ *
+ */
+public class Language {
+
+	private static ResourceBundle messages;	//holds string values
+	private static Locale currentLocale;	//holds users current locaton and language
+	
+	/**
+	 * Chooses what language bundle to use
+	 * 
+	 * @param args to specify what language user want to use, if not supplied the computers default language will be chosen
+	 */
+	public static void setLocale(String[] args){
+		
+		//if user decideds a language on launch
+		if (args.length==2) {
+			currentLocale = new Locale (args[0], args[1]);
+		} else if (args.length==1) {
+			currentLocale = new Locale (args[0]);
+		} else	//else the computers default language 
+			currentLocale = Locale.getDefault();
+			
+		//get correct lanugage bundle
+		messages = ResourceBundle.getBundle("MessagesBundle", currentLocale);
+	}
+	
+	/**
+	 * Function for geting string resourses based on the resourse key
+	 * 
+	 * @param key The resource key to get
+	 * @return The string correct language string
+	 */
+	public static String getMsg(String key){
+		return messages.getString(key);
+	}
+
+}
---
+++ java_irc_client/src/irc_client/main.java
@@ -0,0 +1,122 @@
+package irc_client;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import irc_client.Language;
+import irc_client.client;
+
+
+
+
+
+public class main extends JFrame{
+private client client;
+private JFrame frame;
+	private main(){
+	 frame = new JFrame("");
+	client=new client(frame);
+		fileMenu();
+		
+		
+		
+	}
+	
+	
+public void newTab(){
+	 
+	client.newTab("ddd", 0);
+	client.newTab("ff", 1);
+	client.newTab("dd", 2);
+}
+	
+	 public static void main(String[] args) {
+		 
+	     Language.setLocale(args);
+	        //Schedule a job for the event dispatch thread:
+	        //creating and showing this application's GUI.
+	        SwingUtilities.invokeLater(new Runnable(){
+	            public void run(){
+	                //Turn off metal's use of bold fonts
+	             UIManager.put("swing.boldMetal", Boolean.FALSE);
+	              new main().newTab();
+	             
+	            }
+	        });
+	    }
+	 
+	 public void fileMenu() {
+
+         JMenuBar menuBar = new JMenuBar();
+
+         // Add the menubar to the frame
+         frame.setJMenuBar(menuBar);
+
+         // Define and add two drop down menu to the menubar
+         JMenu fileMenu = new JMenu(Language.getMsg("file"));
+         JMenu editMenu = new JMenu(Language.getMsg("edit"));
+         menuBar.add(fileMenu);
+         menuBar.add(editMenu);
+
+         // Create and add simple menu item to one of the drop down menu
+         JMenuItem newAction = new JMenuItem(Language.getMsg("new"));
+         JMenuItem openAction = new JMenuItem(Language.getMsg("open_file"));
+         JMenuItem saveAction = new JMenuItem(Language.getMsg("save_file"));
+         JMenuItem exitAction = new JMenuItem(Language.getMsg("exit"));
+         JMenuItem genAction = new JMenuItem(Language.getMsg("export"));
+         JMenuItem newItemAction = new JMenuItem(Language.getMsg("add"));
+         JMenuItem deleteItemAction = new JMenuItem(Language.getMsg("delete"));
+
+         // file menu buttons
+         fileMenu.add(newAction);
+         fileMenu.add(openAction);
+         fileMenu.add(saveAction);
+         fileMenu.addSeparator();
+         fileMenu.add(genAction);
+         fileMenu.add(exitAction);
+
+         // edit menu buttons
+         editMenu.add(newItemAction);
+         editMenu.add(deleteItemAction);
+
+         // file menu actions
+         newAction.addActionListener(new ActionListener() {
+                 public void actionPerformed(ActionEvent ae) {
+                         
+                       
+                 }
+         });
+
+         openAction.addActionListener(new Load());
+
+         saveAction.addActionListener(new Save());
+
+         genAction.addActionListener(new SaveReport());
+
+         exitAction.addActionListener(new ActionListener() {
+                 public void actionPerformed(ActionEvent ae) {
+                         System.exit(0);
+                 }
+         });
+
+         // edit menu button actions
+         newItemAction.addActionListener(new ActionListener() {
+                 public void actionPerformed(ActionEvent ae) {
+                       
+                 }
+         });
+
+         deleteItemAction.addActionListener(new ActionListener() {
+                 public void actionPerformed(ActionEvent ae) {
+                         
+                 }
+         });
+
+ }
+}
