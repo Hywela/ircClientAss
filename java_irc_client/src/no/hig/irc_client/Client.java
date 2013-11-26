9ad26c6c4fd680ab6b4da3b8910f2b6059acc5bb
Hywela <kriss_kb@hotmail.com>
2013-11-26 19:01:35 +0100

    Input vs output if you are reading this please skip to another project to steal from*_*

--- java_irc_client/src/no/hig/irc_client/Client.java
+++ java_irc_client/src/no/hig/irc_client/Client.java
@@ -1,189 +1,209 @@
 package no.hig.irc_client;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.Serializable;
-import java.util.List;
 import java.util.Vector;
 
+import javax.swing.AbstractButton;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JSplitPane;
 import javax.swing.JTabbedPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.ScrollPaneConstants;
-
-import jerklib.Channel;
 import jerklib.Session;
-import jerklib.events.ErrorEvent;
-import jerklib.events.IRCEvent;
-import jerklib.events.MessageEvent;
-import jerklib.events.NickListEvent;
-import jerklib.events.QuitEvent;
-import jerklib.events.IRCEvent.Type;
-import jerklib.events.JoinCompleteEvent;
-import jerklib.listeners.IRCEventListener;
-import jerklib.tasks.TaskImpl;
-import no.hig.irc_client.DeleteTabs;
-
-
-
 
 public final class Client implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    private static Vector<String> pmVec ;
-    private static class clientLoader {
-        private static final Client INSTANCE = new Client();
-    }
+	private static final long serialVersionUID = 1L;
+	private static Vector<String> pmVec;
 
-    private Client() {
-        if (clientLoader.INSTANCE != null) {
-            throw new IllegalStateException("Already instantiated");
-        }
-    }
+	private static class clientLoader {
+		private static final Client INSTANCE = new Client();
+	}
 
-    public static Client getInstance() {
-        return clientLoader.INSTANCE;
-    }
+	private Client() {
+		if (clientLoader.INSTANCE != null) {
+			throw new IllegalStateException("Already instantiated");
+		}
+	}
 
-    @SuppressWarnings("unused")
-    private Client readResolve() {
-        return clientLoader.INSTANCE;
-    }
-    
-    public void serFrame(JFrame frame){
-    	pmVec = new Vector<String>();
-    	  this.frame = frame;
-          this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-          b.addActionListener(new java.awt.event.ActionListener() {
+	public static Client getInstance() {
+		return clientLoader.INSTANCE;
+	}
 
-              public void actionPerformed(java.awt.event.ActionEvent evt) {
-                  
-                  if (con == null){
-                  	con = new Connector();
-                  	session = con.getSession();
-                  	connectTab();	
-                  	}else{ session.close("close");}
-                  
-              }
-          });
-      
-       
-          this.frame.add(b,BorderLayout.NORTH);
-          this.frame.add(pane);
-          frame.setSize(new Dimension(700, 300));
-          frame.setLocationRelativeTo(null);
-          frame.setVisible(true);
-    	
-    	
-    }
+	@SuppressWarnings("unused")
+	private Client readResolve() {
+		return clientLoader.INSTANCE;
+	}
 
+	public void serFrame(JFrame frame) {
+		pmVec = new Vector<String>();
+		this.frame = frame;
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		b.addActionListener(new java.awt.event.ActionListener() {
 
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
 
-    
+				if (con == null) {
+					con = new Connector();
+					session = con.getSession();
+					connectTab();
+				} else {
+					session.close("close");
+				}
 
+			}
+		});
 
-	Connector con = null; 
+		this.frame.add(b, BorderLayout.NORTH);
+		this.frame.add(pane);
+		frame.setSize(new Dimension(700, 300));
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+
+	}
+
+	Connector con = null;
 	Session session = null;
 	private JFrame frame;
-    private final JTabbedPane pane = new JTabbedPane();
-    private final JButton b = new JButton("Connect");
-    
+	private final JTabbedPane pane = new JTabbedPane();
+	private final JButton b = new JButton("Connect");
 
-    public Client(JFrame frame) {
-    	
-        this.frame = frame;
-        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        b.addActionListener(new java.awt.event.ActionListener() {
+	public Client(JFrame frame) {
 
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                
-                if (con == null){
-                	con = new Connector();
-                	session = con.getSession();
-                	connectTab();	
-                	}else{ session.close("close");}
-                
-            }
-        });
-    
-     
-        this.frame.add(b,BorderLayout.NORTH);
-        this.frame.add(pane);
-        frame.setSize(new Dimension(700, 300));
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-    }	
+		this.frame = frame;
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		b.addActionListener(new java.awt.event.ActionListener() {
 
-    public void newTab(Session sess, String channel) {
-    		
-    	 
-  
-    	
-    	 Tabs p = new Tabs(new BorderLayout(),true, sess, channel);
-            pane.add(channel,p);
-            initTabComponent();  
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
 
-        pane.setTabLayoutPolicy(JTabbedPane.WRAP_TAB_LAYOUT);
-        
+				if (con == null) {
+					con = new Connector();
+					session = con.getSession();
+					connectTab();
+				} else {
+					session.close("close");
+				}
 
-    
-    }
-    
-    private void connectTab(){
-    	
-    	 Tabs p = new Tabs(new BorderLayout(), false, session, "");
-         pane.add("Connector",p);
-         initTabComponent();  
-         
-  
-      
-     pane.setTabLayoutPolicy(JTabbedPane.WRAP_TAB_LAYOUT);
-    }
-    
-    private void initTabComponent() {
-        pane.setTabComponentAt(pane.getTabCount()-1,
-                 new DeleteTabs(pane));
-    }
+			}
+		});
 
-
-  
-      
-	
-public void joinChannel(String chan){
-	
-	 session.join(chan);
-	 newTab(session, chan );
-}
-  
-public void newPrivatTab(Session s, String nick){
-  	 PrivateMessage p = new PrivateMessage(new BorderLayout(), s, nick);
-     pane.add(nick,p);
-     initTabComponent();  
-     pmVec.add(nick);
- pane.setTabLayoutPolicy(JTabbedPane.WRAP_TAB_LAYOUT);
-	
-}
-public void deleteNick(String s){
-	
-	for (int i = 0; i < pmVec.size(); i++){
-		if(pmVec.get(i).equals(s))
-			pmVec.remove(i);
+		this.frame.add(b, BorderLayout.NORTH);
+		this.frame.add(pane);
+		frame.setSize(new Dimension(700, 300));
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 	}
-	
-}
-public boolean findNick(String s){
-	for (int i = 0; i < pmVec.size(); i++){
-		if(pmVec.get(i).equals(s))
-			return true;
+
+	public void newTab(Session sess, String channel) {
+
+		Tabs p = new Tabs(new BorderLayout(), true, sess, channel);
+		pane.add(channel, p);
+		initiCLoseTabCode(channel);
+
 	}
-	return false;
-}
-    
+
+	private void connectTab() {
+
+		Tabs p = new Tabs(new BorderLayout(), false, session, "");
+		pane.addTab("connector", p);
+		initiCLoseTabCode("connector");
+	}
+
+	public void joinChannel(String chan) {
+
+		session.join(chan);
+		newTab(session, chan);
+	}
+
+	public void newPrivatTab(Session s, String nick) {
+		PrivateMessage p = new PrivateMessage(new BorderLayout(), s, nick);
+		pane.add(nick, p);
+
+		pmVec.add(nick);
+		initiCLoseTabCode(nick);
+		pane.setTabLayoutPolicy(JTabbedPane.WRAP_TAB_LAYOUT);
+
+	}
+
+	public void deleteNick(String s) {
+
+		for (int i = 0; i < pmVec.size(); i++) {
+			if (pmVec.get(i).equals(s))
+				pmVec.remove(i);
+		}
+
+	}
+
+	public boolean findNick(String s) {
+		for (int i = 0; i < pmVec.size(); i++) {
+			if (pmVec.get(i).equals(s))
+				return true;
+		}
+		return false;
+	}
+
+	public void initiCLoseTabCode(String chan) {
+
+		JPanel pnlTab = new JPanel(new GridBagLayout());
+		pnlTab.setOpaque(false);
+		JLabel lblTitle = new JLabel(chan);
+		JButton btnClose = new CloseButton();
+
+		pnlTab.add(lblTitle);
+		lblTitle.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 5));
+
+		pnlTab.add(btnClose);
+
+		btnClose.setBorder(BorderFactory.createEmptyBorder(2, 0, 0, 0));
+
+		pane.setTabComponentAt(pane.getTabCount() - 1, pnlTab);
+
+		pane.setTabLayoutPolicy(JTabbedPane.WRAP_TAB_LAYOUT);
+		btnClose.addMouseListener(buttonMouseListener);
+		btnClose.addActionListener(actionListerner);
+	}
+
+	private final static MouseListener buttonMouseListener = new MouseAdapter() {
+		public void mouseEntered(MouseEvent e) {
+			Component component = e.getComponent();
+			if (component instanceof AbstractButton) {
+				AbstractButton button = (AbstractButton) component;
+				button.setBorderPainted(true);
+			}
+		}
+
+		public void mouseExited(MouseEvent e) {
+			Component component = e.getComponent();
+			if (component instanceof AbstractButton) {
+				AbstractButton button = (AbstractButton) component;
+				button.setBorderPainted(false);
+
+			}
+
+		}
+
+	};
+	public ActionListener actionListerner = new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			int index = pane.getSelectedIndex();
+			if (index >= 1) {
+
+				pane.remove(index);
+			}
+		}
+	};
+
 }
--- java_irc_client/src/no/hig/irc_client/Tabs.java
+++ java_irc_client/src/no/hig/irc_client/Tabs.java
@@ -27,214 +27,212 @@ import jerklib.events.NoticeEvent;
 import jerklib.events.PartEvent;
 import jerklib.events.QuitEvent;
 import jerklib.listeners.IRCEventListener;
-public class Tabs extends JPanel implements IRCEventListener{
-	 private JTextField inputField;
-	 public TextArea text;
-	 
-	 public String channel;
-	 Channel chan; 
-	 DefaultListModel<String> listModel;
-	 JList<DefaultListModel<String>> list;
-	
+
+public class Tabs extends JPanel implements IRCEventListener {
+	private JTextField inputField;
+	public TextArea text;
+
+	public String channel;
+	Channel chan;
+	DefaultListModel<String> listModel;
+	JList<DefaultListModel<String>> list;
+
 	boolean notChat;
-	 public Tabs(BorderLayout borderLayout, boolean type,   final Session s, final String channel) {
+
+	public Tabs(BorderLayout borderLayout, boolean type, final Session s,
+			final String channel) {
 		// TODO Auto-generated constructor stub
-		 super(borderLayout);
-		 listModel = new DefaultListModel<String>();
-		 text = new TextArea();
-     	 inputField= new JTextField();
-     
-     	
-     	
-     	 this.channel = channel;
-     	 chan = new Channel(channel, s);
-		 if(type == false){
-  		
-  		   notChat = true;
-  		 
-     		text.setEditable(false);
-     		JScrollPane scrollPane = new JScrollPane(text,ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
-	        ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-     		scrollPane.setAutoscrolls(true);
-     	
- 
-     		//  split.add(inputField,JSplitPane.BOTTOM);
-     		
-            add(scrollPane,BorderLayout.CENTER);
-            
-           //  p.add(,BorderLayout.CENTER);
-            add(inputField,BorderLayout.SOUTH);
-           
-             }
-    	
-    	
-		 if(type == true){	  
-			
-    
-    	notChat = false;
-    	list = new JList(listModel);
-      	 text.write("Joining Channel : "+ channel, Color.BLUE);
-      	 
-      	text.setEditable(false);
-      	
-      	 
-		JScrollPane scrollPane = new JScrollPane(list,ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
-		        ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		
-		
-		JScrollPane scrollPane2 = new JScrollPane(text,ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
-		        ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		///p.add(scrollPane, BorderLayout.EAST);
-		
-		  JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
-				  scrollPane, scrollPane2);
-		//  split.add(inputField,JSplitPane.BOTTOM);
-     //   p.add(list,BorderLayout.EAST);
-		 add(split);
-      //  p.add(,BorderLayout.CENTER);
-        add(inputField,BorderLayout.SOUTH);
-      
-        }
-		 inputField.addActionListener(new ActionListener() {
-				
-				@Override
-				public void actionPerformed(ActionEvent b) {
-					// TODO Auto-generated method stub
-					inputField.setText("");
-					text.write(b.getActionCommand(),Color.BLACK );
-					
-					System.out.println(b.getActionCommand());
-					
-					 if (b.getActionCommand().startsWith("/")){
-						
-					 String sw = b.getActionCommand().substring(1, 2);
-					 String message = b.getActionCommand().substring(3); 
-					switch(sw){
-					case "j":{
-							Client client = null;
+		super(borderLayout);
+		listModel = new DefaultListModel<String>();
+		text = new TextArea();
+		inputField = new JTextField();
+
+		this.channel = channel;
+		chan = new Channel(channel, s);
+		if (type == false) {
+
+			notChat = true;
+
+			text.setEditable(false);
+			JScrollPane scrollPane = new JScrollPane(text,
+					ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+					ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+			scrollPane.setAutoscrolls(true);
+
+			// split.add(inputField,JSplitPane.BOTTOM);
+
+			add(scrollPane, BorderLayout.CENTER);
+
+			// p.add(,BorderLayout.CENTER);
+			add(inputField, BorderLayout.SOUTH);
+
+		}
+
+		if (type == true) {
+
+			notChat = false;
+			list = new JList(listModel);
+			text.write("Joining Channel : " + channel, Color.BLUE);
+
+			text.setEditable(false);
+
+			JScrollPane scrollPane = new JScrollPane(list,
+					ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+					ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+			JScrollPane scrollPane2 = new JScrollPane(text,
+					ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+					ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+			// /p.add(scrollPane, BorderLayout.EAST);
+
+			JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+					scrollPane, scrollPane2);
+			// split.add(inputField,JSplitPane.BOTTOM);
+			// p.add(list,BorderLayout.EAST);
+			add(split);
+			// p.add(,BorderLayout.CENTER);
+			add(inputField, BorderLayout.SOUTH);
+
+		}
+		inputField.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent b) {
+				// TODO Auto-generated method stub
+				inputField.setText("");
+				text.write(b.getActionCommand(), Color.BLACK);
+
+				System.out.println(b.getActionCommand());
+
+				if (b.getActionCommand().startsWith("/")) {
+
+					String sw = b.getActionCommand().substring(1, 2);
+					String message = b.getActionCommand().substring(3);
+					switch (sw) {
+					case "j": {
+						Client client = null;
 						client = Client.getInstance();
-					
+
 						client.joinChannel(message);
 						break;
 					}
-					
+
 					}
-					
-					
-				}else 
+
+				} else
 					chan.say(b.getActionCommand());
-				}
-			}); 
-		 s.addIRCEventListener(this);
-	 }//END CONSTRUCTORd
-
-
+			}
+		});
+		s.addIRCEventListener(this);
+	}// END CONSTRUCTORd
 
 	@Override
 	public void receiveEvent(IRCEvent e) {
 		// TODO Auto-generated method stub
-		
-		if(e.getType() == Type.JOIN){ 
-			JoinEvent joinEvent = (JoinEvent)e;
-			 Channel gc = joinEvent.getChannel();System.out.println(gc.getName());
-			 if(channel.equals( gc.getName())){
-				 text.write("<"+joinEvent.getNick() + "> has joined the "+gc.getName(),Color.BLUE );
-						listModel.addElement(joinEvent.getNick());
-		}
-			
-		}else if (e.getType() == Type.CHANNEL_MESSAGE){
-			 MessageEvent me = (MessageEvent)e;
-			 Channel gc = me.getChannel();
-			 if(channel.equals( gc.getName())){
-				 
-				 if (!me.getMessage().startsWith("/")){
-				 text.write("<"+me.getNick()+"> "+me.getMessage(),Color.BLACK );
-				 }
-			 }
-		 
-		 if(notChat)
-			 text.write(e.getType() + " " + e.getRawEventData(), Color.BLUE);         
-              
-		}else if (e.getType() == Type.CONNECT_COMPLETE){
-		       System.out.println("CONNECT");
-		}else if (e.getType() == Type.JOIN_COMPLETE){
-			  System.out.println("join");
-	    	  JoinCompleteEvent jce = (JoinCompleteEvent)e;    
-		}else if (e.getType() == Type.SERVER_INFORMATION){
-			 if(notChat)
-				 text.write(e.getType() + " " + e.getRawEventData(), Color.BLUE);  
-		}else if (e.getType() == Type.SERVER_VERSION_EVENT){
-			 if(notChat)
-				 text.write(e.getType() + " " + e.getRawEventData(), Color.BLUE);
-		}else if (e.getType() == Type.CONNECTION_LOST){
-			 if(notChat)
-				 text.write(e.getType() + " " + e.getRawEventData(), Color.BLUE);          	
-				
-			
-		}else if (e.getType() == Type.PRIVATE_MESSAGE){
 
-			System.out.println("hei du er i tabs");
-				Client client = null;
-				client = Client.getInstance();
-           MessageEvent pm = (MessageEvent)e; //message event
-           if(!client.findNick(pm.getNick())){
-        	   client.newPrivatTab(e.getSession(),pm.getNick());
-           } 
-       
-		}else if (e.getType() == Type.PART){
-			 PartEvent partEvent = (PartEvent)e;
-             Channel gc = partEvent.getChannel();System.out.println(gc.getName());
-             if(channel.equals( gc.getName())){
-            		
-       
-					for(int i = 0; i < listModel.getSize(); i++){
-						if( listModel.get(i).toString().equals(partEvent.getWho())){
-							text.write("<"+partEvent.getWho() + "> has quit the "+gc.getName(), Color.RED);
-							
-								listModel.remove(i);}
-						}
-				}
-		}else if (e.getType() == Type.NICK_LIST_EVENT){
-			 NickListEvent ne = ( NickListEvent ) e;
-			 Channel gc = ne.getChannel();
-			 if(channel.equals( gc.getName())){
-				List<String> players = ne.getNicks();
-				for (String nick : players )listModel.addElement(nick); 
-				
+		if (e.getType() == Type.JOIN) {
+			JoinEvent joinEvent = (JoinEvent) e;
+			Channel gc = joinEvent.getChannel();
+			System.out.println(gc.getName());
+			if (channel.equals(gc.getName())) {
+				text.write(
+						"<" + joinEvent.getNick() + "> has joined the "
+								+ gc.getName(), Color.BLUE);
+				listModel.addElement(joinEvent.getNick());
 			}
-		}else if (e.getType() == Type.QUIT){
-			 QuitEvent quitEvent = (QuitEvent)e;
-				
-				if (listModel.getSize() > quitEvent.getChannelList().size()) {
-					for(int i = 0; i>listModel.getSize(); i++){
-						text.write(quitEvent.getNick() + "has quit the Channel", Color.RED);
-						if( listModel.get(i)==quitEvent.getNick()){
-							listModel.remove(i);
-							}
-						}
+
+		} else if (e.getType() == Type.CHANNEL_MESSAGE) {
+			MessageEvent me = (MessageEvent) e;
+			Channel gc = me.getChannel();
+			if (channel.equals(gc.getName())) {
+
+				if (!me.getMessage().startsWith("/")) {
+					text.write("<" + me.getNick() + "> " + me.getMessage(),
+							Color.BLACK);
+				}
+			}
+
+			if (notChat)
+				text.write(e.getType() + " " + e.getRawEventData(), Color.BLUE);
+
+		} else if (e.getType() == Type.CONNECT_COMPLETE) {
+			System.out.println("CONNECT");
+		} else if (e.getType() == Type.JOIN_COMPLETE) {
+			System.out.println("join");
+			JoinCompleteEvent jce = (JoinCompleteEvent) e;
+		} else if (e.getType() == Type.SERVER_INFORMATION) {
+			if (notChat)
+				text.write(e.getType() + " " + e.getRawEventData(), Color.BLUE);
+		} else if (e.getType() == Type.SERVER_VERSION_EVENT) {
+			if (notChat)
+				text.write(e.getType() + " " + e.getRawEventData(), Color.BLUE);
+		} else if (e.getType() == Type.CONNECTION_LOST) {
+			if (notChat)
+				text.write(e.getType() + " " + e.getRawEventData(), Color.BLUE);
+
+		} else if (e.getType() == Type.PRIVATE_MESSAGE) {
+
+			Client client = null;
+			client = Client.getInstance();
+			MessageEvent pm = (MessageEvent) e; // message event
+			if (!client.findNick(pm.getNick())) {
+				client.newPrivatTab(e.getSession(), pm.getNick());
+			}
+
+		} else if (e.getType() == Type.PART) {
+			PartEvent partEvent = (PartEvent) e;
+			Channel gc = partEvent.getChannel();
+			System.out.println(gc.getName());
+			if (channel.equals(gc.getName())) {
+
+				for (int i = 0; i < listModel.getSize(); i++) {
+					if (listModel.get(i).toString().equals(partEvent.getWho())) {
+						text.write("<" + partEvent.getWho() + "> has quit the "
+								+ gc.getName(), Color.RED);
+
+						listModel.remove(i);
 					}
-		}else if (e.getType() == Type.ERROR ){
-			 ErrorEvent errorEvent = (ErrorEvent)e;
-			 text.write(e.getRawEventData(), Color.BLACK);
-			 
-		}else if (e.getType() == Type.INVITE_EVENT){
-			
-			 text.write(e.getRawEventData(), Color.GREEN);
-			 
-		}else if (e.getType() == Type.NOTICE){
-			NoticeEvent event = (NoticeEvent)e;
-			
-			 text.write(event.getNoticeMessage(), Color.RED);
-			
-		}else {
-			
-			 text.write(e.getRawEventData(), Color.RED);
+				}
+			}
+		} else if (e.getType() == Type.NICK_LIST_EVENT) {
+			NickListEvent ne = (NickListEvent) e;
+			Channel gc = ne.getChannel();
+			if (channel.equals(gc.getName())) {
+				List<String> players = ne.getNicks();
+				for (String nick : players)
+					listModel.addElement(nick);
+
+			}
+		} else if (e.getType() == Type.QUIT) {
+			QuitEvent quitEvent = (QuitEvent) e;
+
+			if (listModel.getSize() > quitEvent.getChannelList().size()) {
+				for (int i = 0; i > listModel.getSize(); i++) {
+					text.write(quitEvent.getNick() + "has quit the Channel",
+							Color.RED);
+					if (listModel.get(i) == quitEvent.getNick()) {
+						listModel.remove(i);
+					}
+				}
+			}
+		} else if (e.getType() == Type.ERROR) {
+			ErrorEvent errorEvent = (ErrorEvent) e;
+			text.write(e.getRawEventData(), Color.BLACK);
+
+		} else if (e.getType() == Type.INVITE_EVENT) {
+
+			text.write(e.getRawEventData(), Color.GREEN);
+
+		} else if (e.getType() == Type.NOTICE) {
+			NoticeEvent event = (NoticeEvent) e;
+
+			text.write(event.getNoticeMessage(), Color.RED);
+
+		} else {
+
+			text.write(e.getRawEventData(), Color.RED);
 		}
-			
-		
-		
+
 	}
-	
+
 }
-
-
